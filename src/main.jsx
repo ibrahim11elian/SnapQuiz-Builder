bc51b54b3950cc59aa5d01fb3ff5fffefe763c9f
ibrahim11elian <ibrahim11elian@gmail.com>
2023-12-27 03:28:17 +0200

    change react version to old one to work with codemirror package

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { AppContextProvider } from "./utilities/context.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <AppContextProvider>
     <App />
     <ToastContainer />
-  </AppContextProvider>
+  </AppContextProvider>,
+  document.getElementById("root")
 );
