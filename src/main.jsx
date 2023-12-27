e93d1655ffb5a46e1864d20a64602905a570f878
ibrahim11elian <ibrahim11elian@gmail.com>
2023-12-27 02:48:08 +0200

    remove strict mode since it make the editor appear twice

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -7,10 +6,8 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AppContextProvider>
-      <App />
-      <ToastContainer />
-    </AppContextProvider>
-  </React.StrictMode>
+  <AppContextProvider>
+    <App />
+    <ToastContainer />
+  </AppContextProvider>
 );
